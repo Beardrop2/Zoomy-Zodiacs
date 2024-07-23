93da538bf08bd69e091c00a292eb9f362893b404
interrrp <int@addictedto.coffee>
2024-07-23 18:58:32 +0800

    docs: document tag repositories

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,20 +1,21 @@
 import pytest
 from hypothesis import assume, given
 from hypothesis import strategies as st
-from repositories.tags import suggested_friends
+
+from bot.repositories.tags import suggest_friends
 
 
 @pytest.mark.asyncio()
 async def test_suggested_friends_basic_1() -> None:
     friends = [(5, "a")] * 5 + [(3, "b")] * 4
-    res = await suggested_friends(friends, 1)
+    res = await suggest_friends(friends, 1)
     assert res == {5: ["a"] * 5}
 
 
 @pytest.mark.asyncio()
 async def test_suggested_friends_basic_2() -> None:
     friends = [(1, "a"), (1, "b"), (1, "c"), (2, "c"), (2, "b"), (3, "a")]
-    res = await suggested_friends(friends, 2)
+    res = await suggest_friends(friends, 2)
     assert res == {1: ["a", "b", "c"], 2: ["c", "b"]}
 
 
@@ -22,7 +23,7 @@ async def test_suggested_friends_basic_2() -> None:
 @given(st.lists(st.tuples(st.text(), st.text())), st.integers())
 async def test_suggested_friends_equal(xs: list[tuple[int, str]], amt: int) -> None:
     assume(amt > 0)
-    assert await suggested_friends(xs, amt) == await suggested_friends_old(xs, amt)
+    assert await suggest_friends(xs, amt) == await suggested_friends_old(xs, amt)
 
 
 async def suggested_friends_old(result: list[tuple[int, str]], amt: int) -> dict[int, list[str]]:
