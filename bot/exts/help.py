3a7acf59b39c1ea8c671bc672fc0f5d2e91dc420
Beardrop2 <benjamin.ghwik@gmail.com>
2024-07-26 11:17:39 +1000

    feat: Help config
    I don't want to assume that ollama is running on the local host.
    I don't want to assume we will always use phi3

--- bot/exts/help.py
+++ bot/exts/help.py
@@ -10,6 +10,11 @@ SYSTEM_STRING = "It is essential that you provide answers that are short and con
 class Help(Cog):
     """Cog for helping members with general questions."""
 
+    def __init__(self, bot: Bot, ollama_host: str, ollama_model: str) -> None:
+        self.bot = bot
+        self._host = ollama_host
+        self._model = ollama_model
+
     @slash_command()
     async def help(self, question: str, inter: AppCmdInter) -> None:
         """Get general AI help."""
@@ -19,10 +24,16 @@ class Help(Cog):
             {"role": "system", "content": SYSTEM_STRING},
             {"role": "user", "content": question},
         ]
-        response = await AsyncClient().chat(model="phi3", messages=messages)
+        response = await AsyncClient(host=self._host).chat(model=self._model, messages=messages)
         await inter.send(response)
 
 
 def setup(bot: Bot) -> None:
     """Load the Help cog."""
-    bot.add_cog(Help(bot))
+    bot.add_cog(
+        cog=Help(
+            bot=bot,
+            ollama_host=bot.settings.ollama_host,
+            ollama_model=bot.settings.ollama_model,
+        ),
+    )
