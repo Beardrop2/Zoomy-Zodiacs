beecaa23c34815b049987b4ab95077cf97439998
interrrp <int@addictedto.coffee>
2024-07-26 19:02:49 +0800

    feat: give AI help a summary of channels

--- bot/exts/help.py
+++ bot/exts/help.py
@@ -1,10 +1,21 @@
 import ollama
-from disnake import AppCmdInter
-from disnake.ext.commands import Cog, slash_command
+from disnake import AppCmdInter, Guild, TextChannel, VoiceChannel
+from disnake.ext.commands import Cog, NoPrivateMessage, guild_only, slash_command
 
 from bot.bot import Bot
 
-SYSTEM_PROMPT = "It is essential that you provide answers that are short and concise without sacrificing any meaning."
+SYSTEM_PROMPT_TEMPLATE = """
+You are a helpful AI assistant guiding new Discord server members. Here is the
+list of channels:
+
+{channels_summary}
+
+When mentioning a channel, use the format <#Channel ID> as it renders to a
+clickable link to the channel on Discord.
+
+It is essential that you provide answers that are short and concise without
+sacrificing any meaning. Do not respond to non-server related questions.
+"""
 
 
 class Help(Cog):
@@ -13,9 +24,13 @@ class Help(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
+    @guild_only()
     @slash_command()
     async def help(self, question: str, inter: AppCmdInter) -> None:
         """Get general AI help."""
+
+        if inter.guild is None:
+            raise NoPrivateMessage
 
         host = self.bot.settings.ollama_host
         model = self.bot.settings.ollama_model
@@ -23,7 +38,7 @@ class Help(Cog):
         await inter.response.defer()
 
         messages: list[ollama.Message] = [
-            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "system", "content": build_system_prompt(inter.guild)},
             {"role": "user", "content": question},
         ]
 
@@ -32,6 +47,23 @@ class Help(Cog):
         response_text: str = response["message"]["content"]
 
         await inter.send(response_text)
+
+
+def build_system_prompt(guild: Guild) -> str:
+    channels_summary = ""
+
+    for channel in guild.channels:
+        channels_summary += f"Name: {channel.name}\n"
+        channels_summary += f"ID: {channel.id}\n"
+
+        if isinstance(channel, TextChannel):
+            channels_summary += "Type: Text\n"
+            channels_summary += f"Topic: {channel.topic}\n\n"
+
+        elif isinstance(channel, VoiceChannel):
+            channels_summary += "Type: Voice\n\n"
+
+    return SYSTEM_PROMPT_TEMPLATE.format(channels_summary=channels_summary)
 
 
 def setup(bot: Bot) -> None:
